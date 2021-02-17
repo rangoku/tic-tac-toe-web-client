dfe4b4c9db80ab0a30fd854d1a89d917dacee3e5
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2021-02-17 18:22:59 +0200

    handleClick() refactored

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,28 +1,26 @@
-import React, { MouseEvent } from 'react'
-import ReactDOM from 'react-dom'
+import { MouseEvent } from 'react'
 import { PLAYER_SYMBOL } from '../globals/globals'
 
 import '../styles/board.css'
-import Block from './Block'
 
 export default function Board(): JSX.Element {
 
-    const handleClick = (e: MouseEvent<HTMLButtonElement>, index: string): void => {
-        document.querySelector('#block_' + index)!.innerHTML = PLAYER_SYMBOL
+    const handleClick = (e: any): void => {
+        document.querySelector('#' + e.target.id)!.innerHTML = PLAYER_SYMBOL
     }
 
     return (
         <div className="container">
             <div id="board">
-                <button className="block" id="block_0" onClick={(e) => handleClick(e, '0')}></button>
-                <button className="block" id="block_1" onClick={(e) => handleClick(e, '1')}></button>
-                <button className="block" id="block_2" onClick={(e) => handleClick(e, '2')}></button>
-                <button className="block" id="block_3" onClick={(e) => handleClick(e, '3')}></button>
-                <button className="block" id="block_4" onClick={(e) => handleClick(e, '4')}></button>
-                <button className="block" id="block_5" onClick={(e) => handleClick(e, '5')}></button>
-                <button className="block" id="block_6" onClick={(e) => handleClick(e, '6')}></button>
-                <button className="block" id="block_7" onClick={(e) => handleClick(e, '7')}></button>
-                <button className="block" id="block_8" onClick={(e) => handleClick(e, '8')}></button>
+                <button className="block" id="block_0" onClick={handleClick}></button>
+                <button className="block" id="block_1" onClick={handleClick}></button>
+                <button className="block" id="block_2" onClick={handleClick}></button>
+                <button className="block" id="block_3" onClick={handleClick}></button>
+                <button className="block" id="block_4" onClick={handleClick}></button>
+                <button className="block" id="block_5" onClick={handleClick}></button>
+                <button className="block" id="block_6" onClick={handleClick}></button>
+                <button className="block" id="block_7" onClick={handleClick}></button>
+                <button className="block" id="block_8" onClick={handleClick}></button>
             </div>
         </div>
     )
