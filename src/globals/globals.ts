4d489ff359b3aa445e3d0bfce94e4d10c0196d8d
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2021-02-18 20:43:23 +0200

    Variables refactored & set functions added

--- src/globals/globals.ts
+++ src/globals/globals.ts
@@ -1,7 +1,21 @@
+import { maxHeaderSize } from "http"
+import { PlayerSymbol } from "../types/player"
+
 // config
 export const SERVER_URL = 'http://localhost:5000'
 
 // player
-export let PLAYER_SYMBOL: 'X' | 'O' | 'None' = 'None'
-export let ROOM: string = 'None'
+let PLAYER_SYMBOL: PlayerSymbol
+let ROOM: string | undefined
 
+const setPlayerSymbol = (symbol: PlayerSymbol): void => {
+    PLAYER_SYMBOL = symbol
+}
+
+const setRoom = (room: string | undefined): void => {
+    ROOM = room
+}
+
+
+export { PLAYER_SYMBOL, setPlayerSymbol }
+export { ROOM, setRoom }
