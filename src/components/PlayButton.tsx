87bb791e42f70bdb31a945448b67d67864ef9922
Eugene <Zhenya_Oleynik@users.noreply.github.com>
2021-02-19 14:16:46 +0200

    Cursor styled

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,13 +1,21 @@
+import { useEffect } from 'react'
+import Socket from '../functions/Socket'
 import '../styles/board.css'
 
 const styles_btn: React.CSSProperties = {
     fontSize: 75,
     alignItems: 'center',
     justifyContent: 'center',
-    color: '#06C305'
+    color: '#06C305',
+    cursor: 'pointer'
 }
 
 const PlayButton = (): JSX.Element => {
+
+    useEffect(() => {
+        Socket.initSockets()
+    }, [])
+
     return (
         <div className={"container"}>
             <span style={styles_btn}>Play</span>
