1ce384d1ccc2ec483910beb611b44ebd503adf35
Eugene <oleynik.zh10@gmail.com>
2021-02-23 18:33:56 +0200

    Loading animation added & win\lose message added & winning combination highlights & socket event handlers refactored

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import Loader from 'react-loader-spinner'
+import { setBoardCell } from '../board/board'
 import { handleClick } from '../functions/handleClick'
 import { socket } from '../functions/sockets'
 import { PLAYER_SYMBOL, ROOM } from '../globals/globals'
@@ -19,6 +20,9 @@ const Loading = (): JSX.Element => {
 export default function Board(): JSX.Element {
 
     const [game, setGame] = useState(false)
+    const [isMyMove, setIsMyMove] = useState(true)
+    const [isGameOver, setIsGameOver] = useState(false)
+    const [isWin, setIsWin] = useState(false)
 
     const boardRef = useRef<HTMLDivElement>(null)
 
@@ -32,10 +36,10 @@ export default function Board(): JSX.Element {
     const btn7 = useRef<HTMLButtonElement>(null)
     const btn8 = useRef<HTMLButtonElement>(null)
 
-    let board: Array<JSX.Element> = []
+    let boardMarkup: Array<JSX.Element> = []
 
     for (let i: number = 0; i < 9 /*cells number*/; ++i)
-        board[i] = (
+        boardMarkup[i] = (
             <button
                 key={i}
                 ref={eval(`btn${i}`)}
@@ -45,30 +49,52 @@ export default function Board(): JSX.Element {
             </button>
         )
 
+
     useEffect(() => {
 
         socket.on('start-game', (first_turn: string) => {
             setGame(true)
+
+            if (first_turn !== PLAYER_SYMBOL) {
+                boardRef.current!.style.pointerEvents = 'none'
+                setIsMyMove(false)
+            }
         })
 
-        socket.on('turn', (turn: { position: number, symbol: string }) => {
+        socket.on('turn', (turn: { position: number, symbol: 'X' | 'O' }) => {
+
             eval(`btn${turn.position}`).current!.innerHTML = turn.symbol
 
-            if (turn.symbol !== PLAYER_SYMBOL)
+            if (turn.symbol !== PLAYER_SYMBOL) {
                 boardRef.current!.style.pointerEvents = 'auto'
-            else
+                setIsMyMove(true)
+            }
+            else {
                 boardRef.current!.style.pointerEvents = 'none'
-        })
+                setIsMyMove(false)
+            }
 
+            setBoardCell(turn.position, turn.symbol)
+        })
 
         socket.on('game-over', (
             gameOver: { winner: string, combination: number[] }
         ) => {
-            console.log('game over:', gameOver)
 
-            for (let i: number = 0; i < gameOver.combination.length; ++i) {
+            setIsGameOver(true)
+
+            boardRef.current!.style.pointerEvents = 'none'
+
+            document.querySelector('body')?.addEventListener('click', () => {
+                window.location.href = '/'
+            })
+
+            for (let i: number = 0; i < gameOver.combination.length; ++i)
                 eval(`btn${gameOver.combination[i]}`).current!.style.color = 'yellow'
-            }
+
+            setIsWin(
+                gameOver.winner === PLAYER_SYMBOL ? true : false
+            )
 
         })
 
@@ -79,7 +105,18 @@ export default function Board(): JSX.Element {
             <div className="container">
                 {
                     game ?
-                        <div id="board" ref={boardRef}>{board}</div>
+                        <>
+                            <h1 className="__default__color-green" style={{ marginBottom: '2rem' }}>
+                                {
+                                    isGameOver ?
+                                        isWin ?
+                                            <span>You win!</span> : <span>You lose!</span>
+                                        :
+                                        isMyMove ? <span>Your turn</span> : <span>Opponent's turn</span>
+                                }
+                            </h1>
+                            <div id="board" ref={boardRef}>{boardMarkup}</div>
+                        </>
                         : <Loading />
                 }
             </div>
